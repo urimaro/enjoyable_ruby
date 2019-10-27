9e6573f70ee731e7c255d9d6e9e593db826f0756
Goh Matsumoto <urimaro@gmail.com>
2019-10-28 00:04:32 +0900

    P117

--- 6.11/ten_lines_grep.rb
+++ 6.11/ten_lines_grep.rb
@@ -1,7 +1,7 @@
 pattern = Regexp.new(ARGV[0])
 filename = ARGV[1]
 
-max_matches = 3      # 出力する最大数
+max_matches = 10     # 出力する最大数
 matches = 0          # マッチした行数
 file = File.open(filename)
 file.each_line do |line|
