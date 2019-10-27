3795ce2c79258c4958ed306727ef7db246cc93d6
Goh Matsumoto <urimaro@gmail.com>
2019-10-27 16:37:52 +0900

    P95

---
+++ 5.6/case_class.rb
@@ -0,0 +1,11 @@
+array = ["a", 2, nil]
+array.each do |item|
+  case item
+  when String
+    puts "item is a String."
+  when Numeric
+    puts "item is a Numeric."
+  else
+    puts "item is something."
+  end
+end
