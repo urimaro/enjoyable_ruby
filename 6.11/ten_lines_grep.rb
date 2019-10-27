1cd077c83f7cab0af29e15dc83184d87378608b8
Goh Matsumoto <urimaro@gmail.com>
2019-10-28 00:02:54 +0900

    P117

---
+++ 6.11/ten_lines_grep.rb
@@ -0,0 +1,16 @@
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
+
+max_matches = 3      # 出力する最大数
+matches = 0          # マッチした行数
+file = File.open(filename)
+file.each_line do |line|
+  if matches >= max_matches
+    break
+  end
+  if pattern =~ line
+    matches += 1
+    print line
+  end
+end
+file.close
