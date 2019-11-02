c968ec90e261201804f96b2a4d696da1c25f318a
Goh Matsumoto <urimaro@gmail.com>
2019-11-02 19:01:24 +0900

    P159

--- 8.3/access_test.rb
+++ 8.3/access_test.rb
@@ -1,15 +1,15 @@
 class AccessTest
+  public
+
   def pub
     puts "pub is a public method."
   end
 
-  public :pub
+  private
 
   def priv
     puts "priv is a private method."
   end
-
-  private :priv
 end
 
 access = AccessTest.new
