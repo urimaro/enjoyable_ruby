b496f86c6f38a7e012924a2a384008628e8a66df
Goh Matsumoto <urimaro@gmail.com>
2019-11-02 18:50:19 +0900

    P158 8.3 メソッドの呼び出しを制限する

---
+++ 8.3/access_test.rb
@@ -0,0 +1,17 @@
+class AccessTest
+  def pub
+    puts "pub is a public method."
+  end
+
+  public :pub
+
+  def priv
+    puts "priv is a private method."
+  end
+
+  private :priv
+end
+
+access = AccessTest.new
+access.pub
+access.priv
