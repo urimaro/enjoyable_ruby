2517a95b4c318951af0154a397575b3746290534
Goh Matsumoto <urimaro@gmail.com>
2019-11-04 16:37:52 +0900

    P175

---
+++ 8.10/class_including_some_modules_sample.rb
@@ -0,0 +1,24 @@
+module M1
+  def meth
+    "M1#meth"
+  end
+end
+
+module M2
+  def meth
+    "M2#meth"
+  end
+end
+
+class C
+  include M1
+  include M2
+
+  def meth
+    "C#meth"
+  end
+end
+
+c = C.new
+p c.meth
+p C.ancestors
