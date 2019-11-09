5bb5f564cd8fb558766ba8d593ae05adc14b0d06
Goh Matsumoto <urimaro@gmail.com>
2019-11-09 17:44:40 +0900

    P201

--- 9.6/point.rb
+++ 9.6/point.rb
@@ -16,6 +16,21 @@ class Point
   def -(other)
     self.class.new(x - other.x, y - other.y)
   end
+
+  def +@
+    # 自分の複製を返す
+    dup
+  end
+
+  def -@
+    # x、yのそれぞれの正負を逆にする
+    self.class.new(-x, -y)
+  end
+
+  def ~@
+    # 90度反転させた座標を返す
+    self.class.new(-y, x)
+  end
 end
 
 point0 = Point.new(3, 6)
@@ -25,3 +40,8 @@ p point0
 p point1
 p point0 + point1
 p point0 - point1
+
+point = Point.new(3, 6)
+p +point
+p -point
+p ~point
