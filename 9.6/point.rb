39b85d16fc6bd24f3cf721673ebb6a5acd278f3b
Goh Matsumoto <urimaro@gmail.com>
2019-11-09 18:05:15 +0900

    P202

--- 9.6/point.rb
+++ 9.6/point.rb
@@ -31,6 +31,28 @@ class Point
     # 90度反転させた座標を返す
     self.class.new(-y, x)
   end
+
+  def [](index)
+    case index
+    when 0
+      x
+    when 1
+      y
+    else
+      raise ArgumentError, "out of range `#{index}`"
+    end
+  end
+
+  def []=(index, val)
+    case index
+    when 0
+      self.x = val
+    when 1
+      self.y = val
+    else
+      raise ArgumentError, "out of range `#{index}`"
+    end
+  end
 end
 
 point0 = Point.new(3, 6)
@@ -45,3 +67,11 @@ point = Point.new(3, 6)
 p +point
 p -point
 p ~point
+
+point = Point.new(3, 6)
+p point[0]
+p point[1] = 2
+puts "# point.[]=(1, 2)"
+p point.[]=(1, 2)
+p point[1]
+p point[2]
