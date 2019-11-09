ac6bf3db538ce1a160a6fc7758cf98cbd21d9710
Goh Matsumoto <urimaro@gmail.com>
2019-11-09 17:28:06 +0900

    P199

---
+++ 9.6/point.rb
@@ -0,0 +1,27 @@
+class Point
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x, @y = x, y
+  end
+
+  def inspect
+    "(#{x}, #{y})"
+  end
+
+  def +(other)
+    self.class.new(x + other.x, y + other.y)
+  end
+
+  def -(other)
+    self.class.new(x - other.x, y - other.y)
+  end
+end
+
+point0 = Point.new(3, 6)
+point1 = Point.new(1, 8)
+
+p point0
+p point1
+p point0 + point1
+p point0 - point1
