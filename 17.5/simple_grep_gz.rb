141f6457fa3d25f5040100a2a1f238fe11dcfcfc
Goh Matsumoto <urimaro@gmail.com>
2019-12-15 14:22:18 +0900

    P381 17.5 コマンドとのやりとり

---
+++ 17.5/simple_grep_gz.rb
@@ -0,0 +1,14 @@
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
+
+if /\.gz$/ =~ filename
+  file = IO.popen("gzcat #{filename}")
+else
+  file = File.open(filename)
+end
+
+file.each_line do |line|
+  if pattern =~ line
+    print line
+  end
+end
