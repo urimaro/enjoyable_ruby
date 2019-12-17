e6bab3e87bab427115b5816221b7113bf63c8a73
Goh Matsumoto <urimaro@gmail.com>
2019-12-17 10:46:14 +0900

    P385 Ch.17 練習問題 (1)の答え合わせ
    `File.open` のブロックにはファイルに対する処理だけを実装すべきと考え、処理を移動した

--- 17.exercise/1.rb
+++ 17.exercise/1.rb
@@ -1,17 +1,17 @@
 def counter(filepath)
-  File.open(filepath) do |file|
-    line_num = word_num = char_num = 0
+  line_num = word_num = char_num = 0
 
+  File.open(filepath) do |file|
     file.each_line do |line|
       line_num += 1
       word_num += line.scan(/[^\s]+/).size
       char_num += line.size
     end
-
-    puts "lines: #{line_num}"
-    puts "words: #{word_num}"
-    puts "chars: #{char_num}"
   end
+
+  puts "lines: #{line_num}"
+  puts "words: #{word_num}"
+  puts "chars: #{char_num}"
 end
 
 counter("./17.exercise/NEWS-2.6.0")
