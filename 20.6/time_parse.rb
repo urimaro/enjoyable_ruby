2f84ce6d44cfc8714947e4e18f7a87a021c51f4a
Goh Matsumoto <urimaro@gmail.com>
2020-01-11 15:19:33 +0900

    P434 20.6 文字列から時刻を取り出す

---
+++ 20.6/time_parse.rb
@@ -0,0 +1,7 @@
+require 'time'
+
+p Time.parse("Sun Jan 11 15:14:01 UTC 2020")
+p Time.parse("Mon, 11 Jan 2020 15:14:01 +09:00")
+p Time.parse("2020/1/11")
+p Time.parse("2020/1/11 15:14:01")
+p Time.parse("R2.1.11")
