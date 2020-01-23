6647613a8a0ffa6c5e5423de13b8fc0c91638fed
Goh Matsumoto <urimaro@gmail.com>
2020-01-23 21:39:13 +0900

    P460

---
+++ 22.1/cut_akage.rb
@@ -0,0 +1,17 @@
+htmlfile = "22.1/akage.html"
+textfile = "22.1/akage.txt"
+
+html = File.read(htmlfile, encoding: "UTF-8")
+
+File.open(textfile, "w:UTF-8") do |f|
+  in_header = true
+  html.each_line do |line|
+    if in_header && /<div class="main_text">/ !~ line
+      next
+    else
+      in_header = false
+    end
+    break if /<div class="after_text">/ =~ line
+    f.write line
+  end
+end
